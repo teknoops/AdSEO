0c0bd3e592c4421cebdd515af66a53a500bff636
teknoops <95921927+teknoops@users.noreply.github.com>
2021-12-10 19:44:56 +0300

    Update levelmaxblock.js

--- levelmaxblock.js
+++ levelmaxblock.js
@@ -1,6 +1,29 @@
 !function(){function f(){var a=document.createElement("div");a.id="levelmaxblock";a.innerHTML='<div class="inner"> <div class="header"> <h2>Reklam Engelleyici Algılandı</h2> </div> <div class="isi"> <p>Lütfen reklam engelleyicinizi devre dışı bırakın</p> <div class="tombol"><button class="1 active">Adblock</button><button class="2">Adblock Plus</button></div> <div class="caranya"> <div class="1 active"> <ol> <li>
-                         
-                         
+ <div class="content">
+      <div class="warn-icon">
+        <span class="icon"><i class="fas fa-exclamation"></i></span>
+      </div>
+    </div>                        
+
+  .content .warn-icon{
+  height: 115px;
+  width: 115px;
+  border-radius: 50%;
+  background: linear-gradient(#9b27ca 0%, #9927cf 0%, #d33639 100%, #f92121 100%);
+}
+.warn-icon .icon{
+  height: 100px;
+  width: 100px;
+  background: #fff;
+  border-radius: inherit;
+}
+.warn-icon .icon i{
+  background: linear-gradient(#9b27ca 0%, #9927cf 0%, #d33639 100%, #f92121 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 50px;
+}                         
                        
 document.body.append(a);document.body.style.overflow="hidden";var b=a.querySelectorAll("button");a.querySelector(".close");var d=a.querySelectorAll(".caranya > div");for(a=0;a<b.length;a++)b[a].addEventListener("click",function(a){a.preventDefault();a=this.getAttribute("class").split(" ")[0];for(var c=0;c<d.length;c++)d[c].classList.remove("active"),b[c].classList.remove("active");b[a-1].classList.add("active");d[a-1].classList.add("active")})}var b=document.createElement("script");b.type="text/javascript";
 b.async=!0;b.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";b.onerror=function(){f();window.adblock=!0};var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(b,e)}();
